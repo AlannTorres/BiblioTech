f29691e2f7e0b59defec0d47b82c87dc9fd72b2f
AlannTorres <alanvictor.torres@gmail.com>
2023-08-19 12:16:19 -0300

    Criação post e get da controller User

---
+++ BiblioTech.Api/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using BiblioTech.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioTech.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : Controller
+{
+    private readonly IUserApplication userApplication;
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return BadRequest("Erro");
+    }
+
+    
+}
